6cbadb1e6f6b02ee800d9e9623ef55c38301fcd3
骆明 <736704198@qq.com>
2020-09-09 11:46:38 +0800

    线程爬虫

--- 03 协程/13 案例_gevent_爬取图片.py
+++ 03 协程/13 案例_gevent_爬取图片.py
@@ -8,6 +8,7 @@ import queue
 import time
 
 q = queue.Queue()
+page_num = 2
 
 def download_url(url, type_='content'):
     """下载页面"""
@@ -20,51 +21,47 @@ def download_url(url, type_='content'):
 
 def get_img_url(start_url):
     """得到该页面所有的图片地址"""
-    url_data = download_url(start_url, 'text')
-    url_html = etree.HTML(url_data)
-    img_url = url_html.xpath("//a[@class='thumbnail vpic_wrap']/img/@bpic")
-    put_task(img_url)
-    next_path = url_html.xpath("//a[@class='next pagination-item ']/@href")[0]
-    next_url = 'https:' + next_path
-    get_img_url(next_url)
+    global page_num
+    if page_num > 0:
+        url_data = download_url(start_url, 'text')
+        url_html = etree.HTML(url_data)
+        img_url = url_html.xpath("//a[@class='thumbnail vpic_wrap']/img/@bpic")
+        put_task(img_url)
+        next_path = url_html.xpath("//a[@class='next pagination-item ']/@href")[0]
+        next_url = 'https:' + next_path
+        page_num = page_num - 1
+        get_img_url(next_url)
 
 def put_task(img_url):
     """将得到的图片地址加入的任务中"""
     if isinstance(img_url, list):
         for i in img_url:
             q.put(i)
-            print("put:", i)
     else:
         q.put(img_url)
-    time.sleep(2)
 
 
 def save_img(save_path):
     """从任务中提取图片地址并且，保存图片到本地"""
-    img_url = q.get()
-    img_content = download_url(img_url, 'content')
-    img_name = img_url.split('/')[-1]
-    img_path = os.path.join(save_path, img_name)
-    print(img_path)
-    with open(img_path, 'wb+') as f:
-        f.write(img_content)
-        print(img_name + '下载完毕！')
+    while True:
+        img_url = q.get()
+        img_content = download_url(img_url, 'content')
+        img_name = img_url.split('/')[-1]
+        img_path = os.path.join(save_path, img_name)
+        print(img_path)
+        with open(img_path, 'wb+') as f:
+            f.write(img_content)
+            print(img_name + '下载完毕！')
 
-def printq(a):
-    print("get:", q.get())
 
 def main():
-
     start_url = 'http://tieba.baidu.com/f?kw=海贼王&ie=utf-8'
     save_path = 'img'
     thread_num = 5
-    thread_ = []
     for i in range(thread_num):
-        thread_.append(threading.Thread(target=printq, args=(save_path, )))
-    for i in thread_:
-        i.start()
+        t = threading.Thread(target=save_img, args=(save_path, ))
+        t.start()
     get_img_url(start_url)
-
 
 if __name__ == '__main__':
     main()
